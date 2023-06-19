68d3ae95c9ad50a5d69cad72465f9a156886fca6
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-19 12:40:15 +0700

    Add Business Model

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Person extends Model
+class Business extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function business(): BelongsTo
+    public function people(): HasMany
     {
-        return $this->belongsTo(Business::class);
+        return $this->hasMany(Person::class);
     }
 }
