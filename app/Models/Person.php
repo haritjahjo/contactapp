46312441452dd03585f90dafce8b4da4e69123fe
Hari Tjahjo <haritjahjo@gmail.com>
2023-06-19 13:31:45 +0700

    N+1 Problems for Business Query

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Person extends Model
 {
     use HasFactory, SoftDeletes;
+    protected $with = ['business'];
 
     public function business(): BelongsTo
     {
